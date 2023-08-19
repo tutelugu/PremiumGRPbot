e6636b574384c5d8498432564ee21e12d0b33717
tutelugu <142389771+tutelugu@users.noreply.github.com>
2023-08-19 18:00:43 +0530

    info.py

--- info.py
+++ info.py
@@ -44,7 +44,7 @@ NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://adityastram:6JOkHSVslwx7Q65Z@cluster0.j2nd3xy.mongodb.net/")
-DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
+DATABASE_NAME = environ.get('DATABASE_NAME', "")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
